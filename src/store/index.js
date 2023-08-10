170070927d77949d700d29ae56152f18469b1809
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-08-10 15:27:41 +0500

    practice updating state

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,19 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
 const songSlice = createSlice({
   name: "song",
   initialState: [],
@@ -17,8 +31,10 @@ const songSlice = createSlice({
 const store = configureStore({
   reducer: {
     songs: songSlice.reducer,
+    movies: movieSlice.reducer,
   },
 });
 
 export { store };
 export const { addSong, removeSong } = songSlice.actions;
+export const { addMovie, removeMovie } = movieSlice.actions;
